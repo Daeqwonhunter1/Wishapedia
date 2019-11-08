7fde09f1c2e9ac7f4b15345a42faf6b24c9b27d2
grewa1 <gbjrmail@gmail.com>
2019-11-08 10:16:29 -0500

    installed routes and tested

--- index.js
+++ index.js
@@ -2,3 +2,22 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+
+
+//Routes
+app.get('/', (req, res) => {
+  res.json({ message: 'It is working' })
+})
+
+app.get('/ping', (req, res) => {
+  res.json({ ping: 'pong' })
+})
+
+app.listen(PORT, () => console.log(`up on port ${PORT}`))
